586e25c95d3e9d0ebd377e5c07e2e6fc5ef9c120
16nastase_C <cristiannastase2015@gmail.com>
2022-03-09 13:30:19 +0000

    menu system start

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -8,6 +8,10 @@ import java.io.RandomAccessFile;
 
 public class FileHandler {
 
+    public static void changeLineToDatabase() {
+        //TODO make it rewrite a comma separated file to make it work with the database//
+    }
+
     public static void appendLine(String fileName, String data) {
         // write text to end of the file
         boolean append = true;
@@ -34,9 +38,6 @@ public class FileHandler {
 
     public static void writeLineAt(String fileName, String data, int start) {
         // overwrite a line from position "start" in the file
-        // doesn't check that the start position is actually
-        // the beginning of the file. This will not behave well
-        // unless every line is the same length
         try (RandomAccessFile rf = new RandomAccessFile(fileName, "rws")) {
             rf.seek(start);
             rf.writeBytes(data);
