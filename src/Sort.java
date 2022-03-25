4ea33eb824038ddd0db0732af75c27035a92f5a4
16nastase_C <cristiannastase2015@gmail.com>
2022-03-25 11:42:26 +0000

    + enter in students in 2d array

--- src/Sort.java
+++ src/Sort.java
@@ -6,7 +6,22 @@ public class Sort {
     public Sort(ArrayList<Student> al) {
         this.students = al;
     }
+/*
+    public ArrayList manual() {
 
+    }
 
+    public ArrayList alphabetical() {
 
+    }
+
+    public ArrayList mf() {
+
+    }
+
+    public ArrayList mfNH() {
+
+    }
+
+ */
 }
